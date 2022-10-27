644fc3c7537c94dac2ad512249886f38af987ce4
Hajar <hajar3la2@gmail.com>
2022-10-27 11:04:57 +0300

    Extra change to test : )

--- Group Profiles/ContentView.swift
+++ Group Profiles/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(hue: 0.731, saturation: 0.164, brightness: 0.956).ignoresSafeArea()
+            VStack{
+                Image("TF")
+                    .resizable()
+                    .frame(width: 200,height:200)
+                    .clipShape(Capsule())
+                Text("Hajar Alhissi")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.713, saturation: 0.288, brightness: 0.554))
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Text("I'm 22 years old, a  Prince Sulatan University fresh graduate, majoring in Computer Science ! ")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.731, saturation: 0.38, brightness: 0.813))
+                    .multilineTextAlignment(.center)
+                Text("Extra Change !!!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.713, saturation: 0.288, brightness: 0.554))
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
