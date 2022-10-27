ed0b3b3b731c5d2ada9d93b69f21d36abaab4138
Mashael <moalharbia22@twq.idserve.net>
2022-10-27 09:03:23 +0300

    Initial Commit

---
+++ Generalsetting/GeneralsettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralsettingApp.swift
+//  Generalsetting
+//
+//  Created by Mashael Alharbi on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeneralsettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
